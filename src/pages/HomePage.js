f3b64a586021aa3bebeec95d402a415942c8b8ff
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-10-26 23:27:04 +0900

    배너 구현

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,19 @@
 import React, { useEffect } from 'react'
 import { movieAction } from '../redux/actions/MovieAction'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import Banner from '../components/Banner'
+import '../style/homepage.css'
 const HomePage = () => {
   const dispatch=useDispatch()
+  const {popularMovies,topRatedMovies,upcomingMovies}=useSelector(state=>state.movie)
+  console.log('home',popularMovies)
+
   useEffect(()=>{
     dispatch(movieAction.getMovies())
   },[])
   return (
-    <div>
-      homepage
+    <div className='homePage'>
+     {popularMovies.results &&<Banner movie={popularMovies.results[0]}/> } 
     </div>
   )
 }
